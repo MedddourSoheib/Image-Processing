bdbc7f8abb45a4ab1a550837530e6d9764d90b26
souheib <pro.soheib@gmail.com>
2025-02-21 01:38:59 +0100

    21/02/2025

---
+++ src/gg.h
@@ -0,0 +1,38 @@
+#ifndef PROJECT_H
+#define PROJECT_H
+
+#include "pngwrap.h"
+
+// Définition de la structure pour complexe
+typedef struct {
+    double Re;   // Partie réelle
+    double Im;   // Partie imaginaire
+} cplx;
+
+// Définition de la structure pour l'image complexe 
+typedef struct {
+    int width;   // Largeur de l'image
+    int height;  // Hauteur de l'image
+    cplx *cdata; // Données de l'image complexe
+} C_image;
+
+
+// Fonction de conversion de l'image réelle vers l'image complexe
+C_image int2cplx(bwimage_t *int_image);
+
+
+// Fonction FFT (Transformation de Fourier) sur l'image complexe
+void fourn(float data[], unsigned long nn[], int ndim, int isign);
+
+// Fonction pour appliquer la FFT à une image complexe
+void apply_fft_nn(C_image *complex_image, float *data, int isign);
+
+// Produit de convolution
+C_image produit_images(C_image image1, C_image image2);
+
+// Fonction de conversion de l'image complexe vers l'image réelle
+bwimage_t cplx2int(C_image *complex_image);
+
+
+
+#endif // PROJECT_H
